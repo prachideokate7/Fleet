595df6c8616c8793a2e2f68dd09857bbaa47b5bd
Yash dhanlobhe <ydhanlobhe1930@gmail.com>
2021-07-04 12:27:21 +0530

    done

--- lib/UI/Patient/PatientSignupScreen.dart
+++ lib/UI/Patient/PatientSignupScreen.dart
@@ -3,9 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:untitled1/UI/Patient/loginBackground.dart';
 import 'package:untitled1/components/rounded_button.dart';
 import 'package:untitled1/components/rounded_input_field.dart';
-
 
 class PatientLoginScreen extends StatefulWidget {
   const PatientLoginScreen({Key? key}) : super(key: key);
@@ -17,6 +17,7 @@ class PatientLoginScreen extends StatefulWidget {
 class _PatientLoginScreenState extends State<PatientLoginScreen> {
   TextEditingController phoneNumber = TextEditingController();
   TextEditingController OTPController = TextEditingController();
+  TextEditingController fullNameController = TextEditingController();
 
   void checkPhoneNumberAndSendOTP() {
     if (phoneNumber.text.length == 10) {
@@ -37,18 +38,15 @@ class _PatientLoginScreenState extends State<PatientLoginScreen> {
   }
 
   Future<void> sentOTP(String phoneNumber) async {
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc("df")
-        .set({"df" : "sdf"});
+    FirebaseFirestore.instance.collection("users").doc("df").set({"df": "sdf"});
     // if (!kIsWeb) {
-    if(true){
-    await FirebaseAuth.instance.verifyPhoneNumber(
+    if (true) {
+      await FirebaseAuth.instance.verifyPhoneNumber(
         phoneNumber: "+91" + phoneNumber,
         verificationCompleted: (PhoneAuthCredential credential) {},
         verificationFailed: (FirebaseAuthException e) {
           Fluttertoast.showToast(msg: e.toString());
-          print("yash"+e.toString());
+          print("yash" + e.toString());
         },
         codeSent: (String verificationId, int? resendToken) {
           Fluttertoast.showToast(msg: "code sent!");
@@ -75,47 +73,52 @@ class _PatientLoginScreenState extends State<PatientLoginScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: isPortrait(context)? portraitWidget(context) : null,
+        child: isPortrait(context) ? portraitWidget(context) : null,
       ),
     );
   }
+
   portraitWidget(BuildContext context) {
-    return ListView(
+    return Stack(
       children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(25 , 45 , 25 , 25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              RoundedInputField(controller: phoneNumber, hintText: 'Phone Number'),
-              RoundedInputField(controller: OTPController, hintText: "OTP",icon: Icons.lock_outline_rounded,),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: RoundedButton(
-                  text: "Send OTP",
-                  press:  checkPhoneNumberAndSendOTP,
-                )
+        loginBackground(),
+        ListView(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height*0.15,),
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage:
+              NetworkImage('https://via.placeholder.com/150'),
+              backgroundColor: Colors.transparent,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  RoundedInputField(
+                      controller: fullNameController, hintText: 'Full Name'),
+                  RoundedInputField(
+                      controller: phoneNumber, hintText: 'Phone Number' , icon:  Icons.phone,),
+                  RoundedInputField(
+                    controller: OTPController, hintText: "OTP", icon: Icons.lock_outline_rounded,),
+                  RoundedButton(
+                    text: "Send OTP", press: checkPhoneNumberAndSendOTP,),
+                  RoundedButton(
+                    text: "Verify", press: () => {},),
+                ],
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: RoundedButton(
-                  text: "Verify",
-                  press:  ()=>{},
-                )
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ],
     );
   }
 
   bool isPortrait(context) {
-    if(MediaQuery.of(context).orientation == Orientation.portrait){
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
       return true;
     }
     return false;
   }
 }
-
