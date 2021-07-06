110b57aef7322cccaa9f97b1223bb4bfa9f26096
Yash dhanlobhe <ydhanlobhe1930@gmail.com>
2021-07-07 01:50:43 +0530

    done

--- lib/UI/Hospital/MainScreen/Add Slots/SelectSlotsScreen.dart
+++ lib/UI/Hospital/MainScreen/Add Slots/SelectSlotsScreen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled1/components/rounded_button.dart';
 
 class SelectSlotsScreen extends StatefulWidget {
   const SelectSlotsScreen({Key? key}) : super(key: key);
@@ -9,43 +11,27 @@ class SelectSlotsScreen extends StatefulWidget {
 }
 
 class _SelectSlotsScreenState extends State<SelectSlotsScreen> {
-  var data;
+  var dataFromPrv;
   late CollectionReference collectionReference;
-  late DocumentReference doc;
-  late DocumentSnapshot documentData;
-  bool flag = true;
+  late Map<String, dynamic> ans= new Map();
+
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments;
-    collectionReference = data["collection"];
-    print("x");
-    doc = getDocumentRef();
-    if(flag){
-      print("HI");
-      getData(doc).then((value) => setState(() {
-        doc = getDocumentRef();
-        flag = false;
-      }));
-    }
-    return slotWidgets(doc);
+    dataFromPrv = ModalRoute.of(context)!.settings.arguments;
+    collectionReference = dataFromPrv["collection"];
+
+    return slotWidgets();
   }
 
-  DocumentReference getDocumentRef() {
+  void setData(Map<String, dynamic> dt) {
     DateTime now = new DateTime.now();
     String today =
         "" + now.day.toString() + now.month.toString() + now.year.toString();
-    return collectionReference
-        .doc(data["phone"])
+    collectionReference
+        .doc(dataFromPrv["phone"])
         .collection("slots")
-        .doc(today);
-  }
-
-  Future<DocumentReference> getData(DocumentReference todayDocRef) async {
-    DocumentSnapshot doc = await todayDocRef.get();
-    if (!doc.exists) {
-      await todayDocRef.set(values());
-    }
-    return todayDocRef;
+        .doc(today)
+        .set(dt);
   }
 
   Map<String, dynamic> values() {
@@ -60,17 +46,158 @@ class _SelectSlotsScreenState extends State<SelectSlotsScreen> {
     return val;
   }
 
-  Widget slotWidgets(DocumentReference doc) {
-    if (documentData != null)
-      return Container(
-      padding: EdgeInsets.all(100),
-      child: Text(documentData.data().toString()),
+  Widget slotWidgets() {
+    return Container(
+      child: ListView(
+        padding: EdgeInsets.all(20),
+        children: getListOfSlots(values()),
+      ),
     );
-    print("n");
-     doc.get().then((value) => setState(() {
-            documentData = value;
-     }));
-
-    return Container();
   }
+
+  getListOfSlots(Map<String, dynamic> dt) {
+    final children = <Widget>[];
+    dt.forEach((key, value) {
+      // print(key);
+      children.add(
+        GestureDetector(
+            onTap: (){
+              if(ans.containsKey(key)){
+                setState(() {
+                  ans.remove(key);
+                });
+              }
+              else{
+                setState(() {
+                  print(key);
+                  ans.putIfAbsent(key, () => 3);
+                });
+              }
+            },
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              color:  ans.containsKey(key)? Colors.green : Colors.redAccent,
+              child: Text(
+                key,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        );
+    });
+    children.add(RoundedButton(
+      press: () {
+        setData(ans);
+        Navigator.of(context).pop();
+      },
+      text: "Add Slots",
+    ));
+    return children;
+  }
+
 }
+
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:untitled1/components/rounded_button.dart';
+//
+// class SelectSlotsScreen extends StatefulWidget {
+//   const SelectSlotsScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   _SelectSlotsScreenState createState() => _SelectSlotsScreenState();
+// }
+//
+// class _SelectSlotsScreenState extends State<SelectSlotsScreen> {
+//   var dataFromPrv;
+//   late CollectionReference collectionReference;
+//   late DocumentReference doc;
+//   late DocumentSnapshot slotsDoc;
+//   bool emptyData = true;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     dataFromPrv = ModalRoute.of(context)!.settings.arguments;
+//     collectionReference = dataFromPrv["collection"];
+//     doc = getDocumentRef();
+//     if (emptyData) {
+//       getData(doc).then((value) => setState(() {
+//             doc = getDocumentRef();
+//             emptyData = false;
+//           }));
+//     }
+//     return slotWidgets(doc);
+//   }
+//
+//   DocumentReference getDocumentRef() {
+//     DateTime now = new DateTime.now();
+//     String today =
+//         "" + now.day.toString() + now.month.toString() + now.year.toString();
+//     return collectionReference
+//         .doc(dataFromPrv["phone"])
+//         .collection("slots")
+//         .doc(today);
+//   }
+//
+//   Future<DocumentReference> getData(DocumentReference todayDocRef) async {
+//     DocumentSnapshot doc = await todayDocRef.get();
+//     if (!doc.exists) {
+//       await todayDocRef.set(values());
+//     }
+//     return todayDocRef;
+//   }
+//
+//   Map<String, dynamic> values() {
+//     Map<String, int> val = new Map();
+//
+//     for (int i = 0; i < 24; i++) {
+//       String x;
+//       x = i < 10 ? "0" + i.toString() : i.toString();
+//       val.putIfAbsent("" + x + ":00 - " + x + ":40", () => 0);
+//     }
+//
+//     return val;
+//   }
+//
+//   Widget slotWidgets(DocumentReference doc) {
+//     try {
+//       Map<String, dynamic>? dt = slotsDoc.data();
+//       return Container(
+//         child: ListView(
+//           padding: EdgeInsets.all(20),
+//           children: getListOfSlots(dt),
+//         ),
+//       );
+//     } catch (exception) {
+//       doc.get().then((value) => setState(() {
+//             slotsDoc = value;
+//           }));
+//     }
+//     return Container();
+//   }
+//
+//   getListOfSlots(Map<String, dynamic>? dt) {
+//     final children = <Widget>[];
+//     sort(dt);
+//     dt!.forEach((key, value) {
+//       children.add(Container(
+//         alignment: Alignment.center,
+//         margin: EdgeInsets.all(10),
+//         padding: EdgeInsets.all(10),
+//         color: kPrimaryColor,
+//         child: Text(key , style: TextStyle(
+//           color: Colors.white,
+//         ),),
+//       ));
+//     });
+//     return children;
+//   }
+//
+//
+//
+// }
