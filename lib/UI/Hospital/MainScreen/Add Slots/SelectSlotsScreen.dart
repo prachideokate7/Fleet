c8f25a1e1b5bed644d75bf4dc5c3e9459fa1f331
Yash dhanlobhe <ydhanlobhe1930@gmail.com>
2021-07-06 21:58:41 +0530

    done

--- lib/UI/Hospital/MainScreen/Add Slots/SelectSlotsScreen.dart
+++ lib/UI/Hospital/MainScreen/Add Slots/SelectSlotsScreen.dart
@@ -11,28 +11,66 @@ class SelectSlotsScreen extends StatefulWidget {
 class _SelectSlotsScreenState extends State<SelectSlotsScreen> {
   var data;
   late CollectionReference collectionReference;
-
+  late DocumentReference doc;
+  late DocumentSnapshot documentData;
+  bool flag = true;
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context)!.settings.arguments;
-    // print(data.toString());
     collectionReference = data["collection"];
-    getData();
-    return Container(
-
-    );
+    print("x");
+    doc = getDocumentRef();
+    if(flag){
+      print("HI");
+      getData(doc).then((value) => setState(() {
+        doc = getDocumentRef();
+        flag = false;
+      }));
+    }
+    return slotWidgets(doc);
   }
 
-  Future<void> getData() async {
+  DocumentReference getDocumentRef() {
     DateTime now = new DateTime.now();
-    String today =  "" + now.day.toString() + now.month.toString() + now.year.toString();
-    DocumentSnapshot doc = await collectionReference.doc(data["phone"]).collection("slots").doc(today).get();
-    // print(doc.toString());
-    // ignore: unnecessary_null_comparison
-    if(!doc.exists){
-      collectionReference.doc(data["phone"]).collection("slots").doc(today).set({
-        "hi":"hi"
-      });
+    String today =
+        "" + now.day.toString() + now.month.toString() + now.year.toString();
+    return collectionReference
+        .doc(data["phone"])
+        .collection("slots")
+        .doc(today);
+  }
+
+  Future<DocumentReference> getData(DocumentReference todayDocRef) async {
+    DocumentSnapshot doc = await todayDocRef.get();
+    if (!doc.exists) {
+      await todayDocRef.set(values());
     }
+    return todayDocRef;
+  }
+
+  Map<String, dynamic> values() {
+    Map<String, int> val = new Map();
+
+    for (int i = 0; i < 24; i++) {
+      String x;
+      x = i < 10 ? "0" + i.toString() : i.toString();
+      val.putIfAbsent("" + x + ":00 - " + x + ":40", () => 0);
+    }
+
+    return val;
+  }
+
+  Widget slotWidgets(DocumentReference doc) {
+    if (documentData != null)
+      return Container(
+      padding: EdgeInsets.all(100),
+      child: Text(documentData.data().toString()),
+    );
+    print("n");
+     doc.get().then((value) => setState(() {
+            documentData = value;
+     }));
+
+    return Container();
   }
 }
